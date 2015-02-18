284b85d3754f2600b5d24dfd2fb5d357299a732c
dimtion <zizou.xena@gmail.com>
2015-02-18 14:07:40 +0100

    Initial commit Features : - Select your server - Select default behavior : private / public sharing - Share simple links from share menu

---
+++ app/src/main/java/com/dimtion/shaarlier/AddActivity.java
@@ -0,0 +1,178 @@
+package com.dimtion.shaarlier;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.Map;
+
+
+public class AddActivity extends Activity {
+    private Map<String, String> coockies;
+    private String token;
+    private String urlShaarli;
+    private String username;
+    private String password;
+    private Boolean privateShare;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // setContentView(R.layout.activity_add);
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        String type = intent.getType();
+
+        // Check if the user had his data validated :
+        SharedPreferences pref = getSharedPreferences(getString(R.string.params), MODE_PRIVATE);
+        urlShaarli  = pref.getString(getString(R.string.p_url_shaarli), "");
+        username    = pref.getString(getString(R.string.p_username), "");
+        password    = pref.getString(getString(R.string.p_password),"");
+        Boolean vld = pref.getBoolean(getString(R.string.p_validated), false);
+        Boolean prv = pref.getBoolean(getString(R.string.p_default_private), true);
+        privateShare = prv;
+        
+        if(urlShaarli.equals("") || username.equals("") || password.equals("") || !vld){
+            // If the is an error, launch the settings :
+            Intent intentLaunchSettings = new Intent(this, MainActivity.class);
+            startActivity(intentLaunchSettings);
+        } else if (Intent.ACTION_SEND.equals(action) && type != null) {
+                if ("text/plain".equals(type)) {
+                    String sharedUrl = intent.getStringExtra(Intent.EXTRA_TEXT);
+                    new HandleAddUrl().execute(sharedUrl);
+                    
+                } else {
+                    Toast.makeText(getApplicationContext(), R.string.add_not_handle, Toast.LENGTH_SHORT).show();
+                }
+        }
+        finish();
+    }
+    
+    //
+    // Class which handle the arrival of a new share url, async
+    //
+    private class HandleAddUrl extends AsyncTask<String, Void, Boolean> {
+        
+        @Override
+        protected Boolean doInBackground(String... url){
+            
+            try {
+                // Connect the user to the site :
+                connect();
+                
+                // TODO : Retrieve title, description...
+                String shareTitle = "";
+                String shareDescription = "";
+                String shareTags = "";
+                
+                // Post the shared url :
+                postLink(url[0], shareTitle, shareDescription, shareTags);
+                
+            } catch (IOException | NullPointerException e){
+                return false;
+            }
+            return true;
+        }
+        
+        @Override
+        protected void onPostExecute(Boolean posted){
+            if (posted){
+                Toast.makeText(getApplicationContext(), R.string.add_success, Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), R.string.add_error, Toast.LENGTH_LONG).show();
+            }
+            
+        }
+        
+        private String getToken() throws IOException {            
+            final String loginFormUrl = urlShaarli.concat("?do=login");
+
+            Connection.Response loginFormPage = Jsoup.connect(loginFormUrl)
+                    .method(Connection.Method.GET)
+                    .execute();
+            Document loginPageDoc = loginFormPage.parse();
+
+            
+            Element tokenElement = loginPageDoc.body().select("input[name=token]").first();
+            coockies = loginFormPage.cookies();
+            return tokenElement.attr("value");
+        }
+        
+        
+        private void connect() throws IOException {
+            final String loginUrl = urlShaarli;
+            token = getToken();
+            
+            // The actual request
+            Connection.Response loginPage = Jsoup.connect(loginUrl)
+                    .followRedirects(true)
+                    .method(Connection.Method.POST)
+                    .cookies(coockies)
+                    .data("login", username)
+                    .data("password", password)
+                    .data("token", token)
+                    .data("returnurl", urlShaarli)
+                    .execute();
+            coockies = loginPage.cookies();
+            Document document = loginPage.parse();
+            Element logoutElement = document.body().select("a[href=?do=logout]").first();
+            logoutElement.attr("href"); // If this fails, you're not connected
+        }
+        
+        
+        private void postLink(String url, String title, String description, String tags) 
+                throws IOException {
+            String encodedShareUrl = URLEncoder.encode(url, "UTF-8");
+            // String encodedShareTitle = URLEncoder.encode(title, "UTF-8");
+            // String encodedShareDescription = URLEncoder.encode(description, "UTF-8");
+            // String encodedShareTags = URLEncoder.encode(tags, "UTF-8");
+            
+            // Get a token and a date :
+            final String postFormUrl = urlShaarli + "?post=" + encodedShareUrl;
+            Connection.Response formPage = Jsoup.connect(postFormUrl)
+                    .followRedirects(true)
+                    .cookies(coockies)
+                    .execute();
+            // coockies = formPage.cookies();
+            Document formPageDoc = formPage.parse();
+            // String debug = formPage.body();
+            Element tokenElement = formPageDoc.body().select("input[name=token]").first();
+            token = tokenElement.attr("value");
+            
+            Element dateElement = formPageDoc.body().select("input[name=lf_linkdate]").first();
+            String date = dateElement.attr("value"); // The server date
+            
+            if(title.equals("")) {
+                Element titleElement = formPageDoc.body().select("input[name=lf_title]").first();
+                title = titleElement.attr("value"); // The title given by shaarli
+            }
+            // The post request :
+            final String postUrl = urlShaarli + "?post=" + encodedShareUrl;
+            Connection postPageConn = Jsoup.connect(postUrl)
+                    .method(Connection.Method.POST)
+                    .cookies(coockies)
+                    .data("save_edit", "Save")
+                    .data("token", token)
+                    .data("lf_tags", tags)
+                    .data("lf_linkdate", date)
+                    .data("lf_url", url)
+                    .data("lf_title", title)
+                    .data("lf_description", description);
+            if (privateShare) postPageConn.data("lf_private","on");
+            Connection.Response postPage = postPageConn.execute(); // Then we post
+
+            // String debug = postPage.body();
+            
+        }
+    }
+    }
---
+++ app/src/main/java/com/dimtion/shaarlier/MainActivity.java
@@ -0,0 +1,227 @@
+package com.dimtion.shaarlier;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.URLUtil;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.util.Map;
+
+
+public class MainActivity extends ActionBarActivity {
+    private Map<String, String> coockies;
+    private String token;
+    private String url_shaarli;
+    private String username;
+    private String password;
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SharedPreferences pref = getSharedPreferences(getString(R.string.params), MODE_PRIVATE);
+        String url = pref.getString(getString(R.string.p_url_shaarli), "http://");
+        String usr = pref.getString(getString(R.string.p_username), "");
+        String pwd = pref.getString(getString(R.string.p_password),"");
+        
+        EditText text;
+        text = (EditText) findViewById(R.id.url_shaarli_input);
+        text.setText(url);
+        text = (EditText) findViewById(R.id.username_input);
+        text.setText(usr);
+        text = (EditText) findViewById(R.id.password_input);
+        text.setText(pwd);
+        
+    }
+
+    public void loginHandler(View view){
+
+        ConnectivityManager connMgr = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        EditText text;
+        text = (EditText) findViewById(R.id.url_shaarli_input);
+        String given_url = text.getText().toString();
+        text = (EditText) findViewById(R.id.username_input);
+        String username = text.getText().toString();
+        text = (EditText) findViewById(R.id.password_input);
+        String password = text.getText().toString();
+        
+        if(!given_url.endsWith("/")){
+            given_url +='/';
+        }
+        if (!(given_url.startsWith("http://") || given_url.startsWith("https://"))){
+            given_url = "http://" + given_url;
+        }
+        final String shaarliUrl = given_url;
+
+        // Is the URL possible ? :
+        if (!URLUtil.isValidUrl(shaarliUrl)) {
+            new AlertDialog.Builder(this)
+                    .setTitle(R.string.error)
+                    .setMessage(R.string.error_url)
+                    .show();
+        } else if (networkInfo == null || !networkInfo.isConnected()) { // Sommes nous connecté à internet ?
+            new AlertDialog.Builder(this)
+                    .setTitle(R.string.error)
+                    .setMessage(R.string.error_internet_connection)
+                    .show();
+        } else {
+            // Si il n'y a pas d'erreur d'input on vérifie que les crédits sont corrects :
+            new CheckShaarli().execute(shaarliUrl, username, password);
+            
+            // On enregistre les crédits :
+            SharedPreferences pref = getSharedPreferences(getString(R.string.params), MODE_PRIVATE);
+            SharedPreferences.Editor editor = pref.edit();
+            editor.putString(getString(R.string.p_url_shaarli), shaarliUrl)
+                    .putString(getString(R.string.p_username), username)
+                    .putString(getString(R.string.p_password), password);
+            editor.apply();
+        }
+    }
+    
+    public void tooglePrivate(View view){
+        boolean isChecked = ((CheckBox) findViewById(R.id.default_private)).isChecked();
+        SharedPreferences pref = getSharedPreferences(getString(R.string.params), MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putBoolean(getString(R.string.p_default_private), isChecked);
+        editor.apply();
+        
+        
+        
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    
+    private void setValidated(Boolean value){
+        SharedPreferences pref = getSharedPreferences(getString(R.string.params), MODE_PRIVATE);
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putBoolean(getString(R.string.p_validated), value);
+        editor.apply();
+    }
+    
+    // TODO : compress CheckCredit and CheckShaarli in one class
+    private class CheckCredit extends AsyncTask<String, Void, Boolean> { 
+        
+        @Override
+        protected Boolean doInBackground(String... params) {
+            final String loginUrl = url_shaarli;
+            try {
+                Connection.Response loginPage = Jsoup.connect(loginUrl)
+                        .method(Connection.Method.POST)
+                        .cookies(coockies)
+                        .data("login", username)
+                        .data("password", password)
+                        .data("token", token)
+                        .data("returnurl", url_shaarli)
+                        .execute();
+                
+                Document document = loginPage.parse();
+                Element logoutElement = document.body().select("a[href=?do=logout]").first();
+                logoutElement.attr("href"); // If this fails, you're not connected
+                coockies = loginPage.cookies();
+            } catch (Exception e) {
+                return false;
+            }
+            return true;
+        }
+        
+        @Override
+        protected void onPostExecute(Boolean is_log_ok) {
+            if (is_log_ok) {
+                Toast.makeText(getApplicationContext(), R.string.success_test, Toast.LENGTH_LONG).show();
+                
+                // On enregistre ce succès :
+                setValidated(true);
+
+            } else {
+                // TODO : préciser l'erreur pour l'utilisateur.
+                Toast.makeText(getApplicationContext(), R.string.login_error, Toast.LENGTH_LONG).show();
+
+                setValidated(false);
+            }
+        }
+        
+    }
+    // Envoie une requete au Shaarli pour vérifier que s'en est bien un.
+    private class CheckShaarli extends AsyncTask<String, Void, Boolean> {
+        // private Exception exception;
+        
+        @Override
+        protected Boolean doInBackground(String... urls) {
+            final String loginFormUrl = urls[0] + "?do=login";
+            try {
+                // On récupère la page du formulaire :
+                Connection.Response loginFormPage = Jsoup.connect(loginFormUrl)
+                        .method(Connection.Method.GET)
+                        .execute();
+                
+                Document loginPageDoc = loginFormPage.parse();
+                Element tokenElement = loginPageDoc.body().select("input[name=token]").first();
+
+                // On conserve les cookies et le token :
+                coockies = loginFormPage.cookies();
+                token = tokenElement.attr("value");
+                url_shaarli = urls[0];
+                username = urls[1];
+                password = urls[2];
+                
+            } catch (Exception e) {
+                return false;
+            }
+            return true;
+        }
+    
+        @Override
+        protected void onPostExecute(Boolean is_log_ok) {
+            if (is_log_ok) {
+                // Toast.makeText(getApplicationContext(), "Token : " + token, Toast.LENGTH_SHORT).show();
+                // Maintenant qu'on a le token on vérifie que mdp-pseudo est correct.
+                new CheckCredit().execute();
+                
+            } else {
+                // TODO : préciser l'erreur pour l'utilisateur.
+                Toast.makeText(getApplicationContext(), R.string.login_error, Toast.LENGTH_LONG).show();
+                
+                setValidated(false);
+                
+            }
+        }
+        
+    }
+    
+}
