dimtion/Shaarlier

commit c24051aede308e41e44e11d119bc8a9ddb428f66

Updated settings windows for better display on small screens.

app/src/main/res/values/strings.xml
@@ -7,7 +7,7 @@
     <string name="user_name">Pseudo :</string>
     <string name="user_password">Mot de passe :</string>
     <string name="more">Plus à venir...</string>
-    <string name="test_connection">Enregistrer</string>
+    <string name="test_connection">Tester les réglages</string>
     <string name="error">Erreur</string>
     <string name="error_url">L\'url fournie n\'est pas valide.</string>
     <string name="error_internet_connection">Vous n\'êtes pas connecté à internet.</string>

.idea/inspectionProfiles/Project_Default.xml
@@ -2,6 +2,14 @@
   <profile version="1.0" is_locked="false">
     <option name="myName" value="Project Default" />
     <option name="myLocal" value="false" />
+    <inspection_tool class="AndroidLintDuplicateIds" enabled="false" level="ERROR" enabled_by_default="false" />
+    <inspection_tool class="AndroidLintNotSibling" enabled="false" level="ERROR" enabled_by_default="false" />
+    <inspection_tool class="AndroidLintRtlHardcoded" enabled="false" level="WARNING" enabled_by_default="false" />
+    <inspection_tool class="AndroidLintUnusedAttribute" enabled="false" level="WARNING" enabled_by_default="false" />
     <inspection_tool class="AndroidLintUselessParent" enabled="false" level="WARNING" enabled_by_default="false" />
+    <inspection_tool class="LoggerInitializedWithForeignClass" enabled="false" level="WARNING" enabled_by_default="false">
+      <option name="loggerClassName" value="org.apache.log4j.Logger,org.slf4j.LoggerFactory,org.apache.commons.logging.LogFactory,java.util.logging.Logger" />
+      <option name="loggerFactoryMethodName" value="getLogger,getLogger,getLog,getLogger" />
+    </inspection_tool>
   </profile>
 </component>

app/src/main/res/layout/activity_main.xml
@@ -1,10 +1,29 @@
-<RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
-    xmlns:tools="http://schemas.android.com/tools" android:layout_width="wrap_content"
-    android:layout_height="match_parent" android:paddingLeft="@dimen/activity_horizontal_margin"
-    android:paddingRight="@dimen/activity_horizontal_margin"
-    android:paddingTop="@dimen/activity_vertical_margin"
-    android:paddingBottom="@dimen/activity_vertical_margin" tools:context=".MainActivity"
-    android:id="@+id/activity_main">
+<?xml version="1.0" encoding="utf-8"?>
+<ScrollView xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:layout_width="fill_parent"
+    android:layout_height="fill_parent">
+
+<LinearLayout 
+    android:layout_width="match_parent"
+    android:orientation="vertical"
+    android:layout_height="wrap_content"
+    tools:context=".MainActivity"
+    android:id="@+id/activity_main"
+    android:paddingTop="20dp"
+    android:paddingBottom="10dp"
+    android:paddingLeft="10dp"
+    android:paddingRight="10dp">
+
+    <ImageView
+        android:layout_width="fill_parent"
+        android:layout_height="100dp"
+        android:id="@+id/imageView"
+        android:src="@drawable/shaarli_icon"
+        android:layout_gravity="top|center_horizontal"
+        android:contentDescription="Shaarli icon"
+        android:cropToPadding="false"
+        android:maxHeight="10dp" />
 
     <TextView
         android:layout_width="wrap_content"
@@ -12,41 +31,27 @@
         android:textAppearance="?android:attr/textAppearanceMedium"
         android:text="@string/your_shaarli_url"
         android:id="@+id/textView"
-        android:layout_alignParentTop="true"
-        android:layout_alignParentLeft="true"
-        android:layout_alignParentStart="true" />
+        android:layout_marginTop="20dp" />
 
     <EditText
-        android:layout_width="wrap_content"
-        android:layout_height="wrap_content"
+        android:layout_width="match_parent"
+        android:layout_height="0dp"
         android:id="@+id/url_shaarli_input"
-        android:layout_below="@+id/textView"
-        android:layout_alignParentLeft="true"
-        android:layout_alignParentStart="true"
-        android:layout_alignParentRight="true"
-        android:layout_alignParentEnd="true"
-        android:inputType="textUri" />
+        android:inputType="textUri"
+        android:layout_weight="1" />
 
     <TextView
         android:layout_width="wrap_content"
         android:layout_height="wrap_content"
         android:textAppearance="?android:attr/textAppearanceMedium"
         android:text="@string/user_name"
-        android:id="@+id/textView2"
-        android:layout_marginTop="25dp"
-        android:layout_below="@+id/url_shaarli_input"
-        android:layout_alignLeft="@+id/username_input"
-        android:layout_alignStart="@+id/username_input" />
+        android:id="@+id/textView"
+        android:layout_marginTop="25dp" />
 
     <EditText
-        android:layout_width="wrap_content"
+        android:layout_width="match_parent"
         android:layout_height="wrap_content"
         android:id="@+id/username_input"
-        android:layout_below="@+id/textView2"
-        android:layout_alignRight="@+id/url_shaarli_input"
-        android:layout_alignEnd="@+id/url_shaarli_input"
-        android:layout_alignParentLeft="true"
-        android:layout_alignParentStart="true"
         android:inputType="textPersonName" />
 
     <TextView
@@ -54,33 +59,15 @@
         android:layout_height="wrap_content"
         android:textAppearance="?android:attr/textAppearanceMedium"
         android:text="@string/user_password"
-        android:id="@+id/textView3"
-        android:layout_below="@+id/username_input"
-        android:layout_alignParentLeft="true"
-        android:layout_alignParentStart="true"
+        android:id="@+id/textView"
         android:layout_marginTop="26dp" />
 
     <EditText
-        android:layout_width="wrap_content"
+        android:layout_width="match_parent"
         android:layout_height="wrap_content"
         android:inputType="textPassword"
         android:ems="10"
-        android:id="@+id/password_input"
-        android:layout_below="@+id/textView3"
-        android:layout_alignParentLeft="true"
-        android:layout_alignParentStart="true"
-        android:layout_alignRight="@+id/username_input"
-        android:layout_alignEnd="@+id/username_input" />
-
-    <TextView
-        android:layout_width="wrap_content"
-        android:layout_height="wrap_content"
-        android:textAppearance="?android:attr/textAppearanceMedium"
-        android:text="@string/more"
-        android:id="@+id/textView4"
-        android:layout_alignParentBottom="true"
-        android:layout_alignParentRight="true"
-        android:layout_alignParentEnd="true" />
+        android:id="@+id/password_input" />
 
     <CheckBox
         android:layout_width="fill_parent"
@@ -88,9 +75,6 @@
         android:text="@string/default_private"
         android:id="@+id/default_private"
         android:checked="true"
-        android:layout_below="@+id/password_input"
-        android:layout_alignParentLeft="true"
-        android:layout_alignParentStart="true"
         android:layout_marginTop="26dp"
         android:onClick="toogglePrivate" />
 
@@ -100,8 +84,18 @@
         android:text="@string/test_connection"
         android:id="@+id/button"
         android:onClick="loginHandler"
-        android:layout_above="@+id/textView4"
-        android:layout_centerHorizontal="true"
-        android:layout_marginBottom="89dp" />
+        android:layout_marginTop="20dp"
+        android:layout_gravity="center_horizontal"
+        android:layout_marginBottom="20dp" />
 
-</RelativeLayout>
+    <TextView
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:textAppearance="?android:attr/textAppearanceMedium"
+        android:text="@string/more"
+        android:id="@+id/textView"
+        android:layout_gravity="end|right"
+        android:layout_marginTop="20dp" />
+</LinearLayout>
+
+</ScrollView>

commit dbe2a33031e309cae1db3557d39ed189fb78bf73

Resolved bug introduced in c24051a  impossible posting due to unchecked url

app/src/main/java/com/dimtion/shaarlier/AddActivity.java
@@ -39,21 +39,28 @@ public class AddActivity extends Activity {
         username    = pref.getString(getString(R.string.p_username), "");
         password    = pref.getString(getString(R.string.p_password),"");
         Boolean vld = pref.getBoolean(getString(R.string.p_validated), false);
-        Boolean prv = pref.getBoolean(getString(R.string.p_default_private), true);
-        privateShare = prv;
+        privateShare = pref.getBoolean(getString(R.string.p_default_private), true);
+
+        // convert urlShaarli into a real url :
+        if(!urlShaarli.endsWith("/")){
+            urlShaarli +='/';
+        }
+        if (!(urlShaarli.startsWith("http://") || urlShaarli.startsWith("https://"))){
+            urlShaarli = "http://" + urlShaarli;
+        }
         
-        if(urlShaarli.equals("") || username.equals("") || password.equals("") || !vld){
+        if(username.equals("") || password.equals("") || !vld){
             // If the is an error, launch the settings :
             Intent intentLaunchSettings = new Intent(this, MainActivity.class);
             startActivity(intentLaunchSettings);
         } else if (Intent.ACTION_SEND.equals(action) && type != null) {
-                if ("text/plain".equals(type)) {
-                    String sharedUrl = intent.getStringExtra(Intent.EXTRA_TEXT);
-                    new HandleAddUrl().execute(sharedUrl);
-                    
-                } else {
-                    Toast.makeText(getApplicationContext(), R.string.add_not_handle, Toast.LENGTH_SHORT).show();
-                }
+            if ("text/plain".equals(type)) {
+                String sharedUrl = intent.getStringExtra(Intent.EXTRA_TEXT);
+                new HandleAddUrl().execute(sharedUrl);
+                
+            } else {
+                Toast.makeText(getApplicationContext(), R.string.add_not_handle, Toast.LENGTH_SHORT).show();
+            }
         }
         finish();
     }
@@ -169,7 +176,7 @@ public class AddActivity extends Activity {
                     .data("lf_title", title)
                     .data("lf_description", description);
             if (privateShare) postPageConn.data("lf_private","on");
-            Connection.Response postPage = postPageConn.execute(); // Then we post
+            postPageConn.execute(); // Then we post
 
             // String debug = postPage.body();
             

app/src/main/java/com/dimtion/shaarlier/MainActivity.java
@@ -54,13 +54,7 @@ public class MainActivity extends ActionBarActivity {
     @Override
    public void onPause(){
         super.onPause();
-        // Save settings :
-        String url = ((EditText) findViewById(R.id.url_shaarli_input)).getText().toString();
-        saveStringPref(R.string.p_url_shaarli, url);
-        String usr = ((EditText) findViewById(R.id.username_input)).getText().toString();
-        saveStringPref(R.string.p_username, usr);
-        String pwd = ((EditText) findViewById(R.id.password_input)).getText().toString();
-        saveStringPref(R.string.p_password, pwd);
+        saveSettings();
     }
 
     public void loginHandler(View view){
@@ -94,29 +88,30 @@ public class MainActivity extends ActionBarActivity {
             new CheckShaarli().execute(shaarliUrl, username, password);
             
             // On enregistre les crédits :
-            SharedPreferences pref = getSharedPreferences(getString(R.string.params), MODE_PRIVATE);
-            SharedPreferences.Editor editor = pref.edit();
-            editor.putString(getString(R.string.p_url_shaarli), shaarliUrl)
-                    .putString(getString(R.string.p_username), username)
-                    .putString(getString(R.string.p_password), password)
-                    .apply();
+            saveSettings();
         }
     }
     
     public void togglePrivate(View view){
-        boolean isChecked = ((CheckBox) findViewById(R.id.default_private)).isChecked();
-        SharedPreferences pref = getSharedPreferences(getString(R.string.params), MODE_PRIVATE);
-        SharedPreferences.Editor editor = pref.edit();
-        editor.putBoolean(getString(R.string.p_default_private), isChecked)
-                .apply();
+        saveSettings();
     }
-    
-    
-    protected void saveStringPref(int key, String data){
+
+    protected void saveSettings(){
+        // Get user inputs :
+        String url = ((EditText) findViewById(R.id.url_shaarli_input)).getText().toString();
+        String username = ((EditText) findViewById(R.id.username_input)).getText().toString();
+        String password = ((EditText) findViewById(R.id.password_input)).getText().toString();
+        boolean isPrivate = ((CheckBox) findViewById(R.id.default_private)).isChecked();
+
+        // Save data :
         SharedPreferences pref = getSharedPreferences(getString(R.string.params), MODE_PRIVATE);
         SharedPreferences.Editor editor = pref.edit();
-        editor.putString(getString(key), data);
-        editor.apply();        
+        editor.putString(getString(R.string.p_url_shaarli), url)
+                .putString(getString(R.string.p_username), username)
+                .putString(getString(R.string.p_password), password)
+                .putBoolean(getString(R.string.p_default_private), isPrivate)
+                .apply();
+        
     }
     
     @Override

app/src/main/res/values/strings.xml
@@ -28,5 +28,5 @@
     <string name="p_default_private">com.dimtion.shaarlier.p_default_private</string>
     <string name="error_connecting">Le site donné ne semble pas fonctionner.</string>
     <string name="error_login">Le pseudo ou le mot de passe semble incorrect.</string>
-    <string name="error_parsing_token">Le site indiqué n\'est pas être un Shaarli. Du moins il n\'est pas géré.</string>
+    <string name="error_parsing_token">Le site indiqué n\'est pas un Shaarli compatible.</string>
 </resources>