dimtion/Shaarlier

commit cd857f23e93f4fb6a970884925a1b26730be789d

Add share action to NetworkService
Now the share action is in a totally different service, it means that it no longer bother the user
when the sharing is ok. Also it will not block the interface.

Also it means that a notification is sent when the share fail, the notification allow the user to
try again his share. For the moment there is one notification for each link, in the future I should
create single notification for all failed shares.

TODO :
 - Allow the user to send me an email if the share fails too many times.
 - Extend the layout of the notification for a better interface
 - Move other smaller network actions to the NetworkService

app/src/main/java/com/dimtion/shaarlier/NetworkService.java
@@ -1,9 +1,14 @@
 package com.dimtion.shaarlier;
 
 import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Message;
 import android.os.Messenger;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
 
 import java.io.IOException;
@@ -14,25 +19,30 @@ public class NetworkService extends IntentService {
     static final int NETWORK_ERROR = 1;
     static final int TOKEN_ERROR = 2;
     static final int LOGIN_ERROR = 3;
+
     private Exception mError;
+    private ShaarliAccount mShaarliAccount;
     public NetworkService() {
         super("NetworkService");
     }
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        // Load the messenger to communicate back to the activity
+        Messenger messenger = (Messenger)intent.getExtras().get(EXTRA_MESSENGER);
+        Message msg = Message.obtain();
         String action = intent.getStringExtra("action");
+
+
         if(action.equals("checkShaarli")){
             String urlShaarli = intent.getStringExtra("urlShaarli");
             String username = intent.getStringExtra("username");
             String password = intent.getStringExtra("password");
-            int result = checkShaarli(urlShaarli, username, password);
-            Messenger messenger = (Messenger)intent.getExtras().get(EXTRA_MESSENGER);
-            Message msg = Message.obtain();
-            msg.arg1 = result;
+            msg.arg1 = checkShaarli(urlShaarli, username, password);
             if(msg.arg1 == NETWORK_ERROR){
                 msg.obj = mError;
             }
+            // Send back messages to the calling activity
             try {
                 assert messenger != null;
                 messenger.send(msg);
@@ -41,7 +51,29 @@ public class NetworkService extends IntentService {
                 Log.w(getClass().getName(), "Exception sending message", e1);
 
             }
+        } else {
+            if (action.equals("postLink")) {
+                String sharedUrl = intent.getStringExtra("sharedUrl");
+                String title = intent.getStringExtra("title");
+                String description = intent.getStringExtra("description");
+                String tags = intent.getStringExtra("tags");
+                boolean isPrivate = intent.getBooleanExtra("privateShare", true);
+                int accountId = intent.getIntExtra("chosenAccountId", -1);
+
+                try {
+                    AccountsSource acs = new AccountsSource(this);
+                    this.mShaarliAccount = (accountId != -1 ? acs.getShaarliAccountById(accountId) : acs.getDefaultAccount());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    sendNotificationShareError(sharedUrl, title, description, tags, isPrivate);
+                    stopSelf();
+                }
+                // TODO : wait for title to be retrieved.
+                postLink(sharedUrl, title, description, tags, isPrivate);
+            }
         }
+
+
         stopSelf();
     }
 
@@ -60,5 +92,77 @@ public class NetworkService extends IntentService {
 
         }
         return NO_ERROR;
+    }
+
+    private void postLink(String sharedUrl, String title, String description, String tags, boolean privateShare){
+        boolean posted = true;  // Assume it is shared
+        try {
+            // Connect the user to the site :
+            NetworkManager manager = new NetworkManager(
+                    mShaarliAccount.getUrlShaarli(),
+                    mShaarliAccount.getUsername(),
+                    mShaarliAccount.getPassword());
+            manager.setTimeout(60000); // Long for slow networks
+            if(manager.retrieveLoginToken() && manager.login()) {
+                manager.postLink(sharedUrl, title, description, tags, privateShare);
+            } else {
+                mError = new Exception("Could not connect to the shaarli. Possibles causes : unhandled shaarli, bad username or password");
+                posted =  false;
+            }
+        } catch (IOException | NullPointerException e) {
+            mError = e;
+            Log.e("ERROR", e.getMessage());
+            posted = false;
+        }
+
+        if (!posted) {
+//            Toast.makeText(getApplicationContext(), R.string.add_error + " : " + mError.getMessage(), Toast.LENGTH_LONG).show();
+            sendNotificationShareError(sharedUrl, title, description, tags, privateShare);
+//            sendReport(mError, chosenAccount);
+        } else {
+            Log.i("SUCCESS", "Success while sharing link");
+        }
+    }
+
+    private void sendNotificationShareError(String sharedUrl, String title, String description, String tags, boolean privateShare){
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_launcher)
+                        .setContentTitle("Failed to share " + title)
+                        .setContentText("Press to try again")
+                        .setAutoCancel(true)
+                        .setPriority(NotificationCompat.PRIORITY_LOW);
+
+        // Creates an explicit intent for an Activity in your app
+        Intent resultIntent = new Intent(this, NetworkService.class);
+
+//        resultIntent.setAction(Intent.ACTION_SEND);
+//        resultIntent.setType("text/plain");
+        resultIntent.putExtra("action", "postLink");
+        resultIntent.putExtra("sharedUrl", sharedUrl);
+        resultIntent.putExtra("title", title);
+        resultIntent.putExtra("description", description);
+        resultIntent.putExtra("tags", tags);
+        resultIntent.putExtra("privateShare", privateShare);
+        resultIntent.putExtra("chosenAccountId", this.mShaarliAccount.getId());
+
+
+        resultIntent.putExtra(Intent.EXTRA_TEXT, sharedUrl);
+        resultIntent.putExtra(Intent.EXTRA_SUBJECT, title);
+
+        // The stack builder object will contain an artificial back stack for the
+        // started Activity.
+        // This ensures that navigating backward from the Activity leads out of
+        // your application to the Home screen.
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        // Adds the Intent that starts the Activity to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent = PendingIntent.getService(this, 0, resultIntent, 0);
+
+        mBuilder.setContentIntent(resultPendingIntent);
+        NotificationManager mNotificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        // mId allows you to update the notification later on.
+        mNotificationManager.notify(sharedUrl.hashCode(), mBuilder.build());
     }
 }

app/src/main/java/com/dimtion/shaarlier/AddActivity.java
@@ -52,7 +52,7 @@ public class AddActivity extends Activity {
         m_prefOpenDialog = pref.getBoolean(getString(R.string.p_show_share_dialog), true);
         autoTitle = pref.getBoolean(getString(R.string.p_auto_title), true);
 
-        // Check if there is at least one account, then launch the settings :
+        // Check if there is at least one account, if so launch the settings :
         getAllAccounts();
         if (this.allAccounts.isEmpty()) {
             Intent intentLaunchSettings = new Intent(this, MainActivity.class);
@@ -64,16 +64,19 @@ public class AddActivity extends Activity {
             String sharedUrlTrimmed = this.extractUrl(sharedUrl);
             String defaultTitle = this.extractTitle(reader);
 
+            String defaultDescription = intent.getStringExtra("description") != null ? intent.getStringExtra("description") : "";
+            String defaultTags = intent.getStringExtra("tags") != null ? intent.getStringExtra("tags") : "";
+
             // Show edit dialog if the users wants :
             if (m_prefOpenDialog) {
-                handleDialog(sharedUrlTrimmed, defaultTitle);
+                handleDialog(sharedUrlTrimmed, defaultTitle, defaultDescription, defaultTags);
             } else {
                 if (autoTitle) {
                     final GetPageTitle getter = new GetPageTitle();
                     a_TitleGetterExec = getter.execute(sharedUrlTrimmed, defaultTitle);
                 }
-
-                new HandleAddUrl().execute(sharedUrlTrimmed, defaultTitle, "", "");
+                handleSendPost(sharedUrlTrimmed, defaultTitle, defaultDescription, defaultTags, privateShare, this.chosenAccount);
+//                new HandleAddUrl().execute(sharedUrlTrimmed, defaultTitle, "", "");
             }
         } else {
             Toast.makeText(getApplicationContext(), R.string.add_not_handle, Toast.LENGTH_SHORT).show();
@@ -167,7 +170,7 @@ public class AddActivity extends Activity {
     //
     // Method made to handle the dialog box
     //
-    private void handleDialog(final String sharedUrl, String givenTitle) {
+    private void handleDialog(final String sharedUrl, String givenTitle, String defaultDescription, String defaultTags) {
         AlertDialog.Builder builder = new AlertDialog.Builder(new ContextThemeWrapper(this, R.style.AppTheme));
         LayoutInflater inflater = AddActivity.this.getLayoutInflater();
         final View dialogView = inflater.inflate(R.layout.share_dialog, null);
@@ -185,8 +188,12 @@ public class AddActivity extends Activity {
         // Init url  :
         ((EditText) dialogView.findViewById(R.id.url)).setText(sharedUrl);
 
+        // Init Description field :
+        ((EditText) dialogView.findViewById(R.id.description)).setText(defaultDescription);
+
         // Init tags :
         MultiAutoCompleteTextView textView = (MultiAutoCompleteTextView) dialogView.findViewById(R.id.tags);
+        ((EditText) dialogView.findViewById(R.id.tags)).setText(defaultTags);
         new AutoCompleteWrapper(textView, this);
 
         // Open the dialog :
@@ -209,7 +216,9 @@ public class AddActivity extends Activity {
 
 
                         // Finally send everything
-                        new HandleAddUrl().execute(url, title, description, tags);
+                        handleSendPost(url, title, description, tags, privateShare, chosenAccount);
+
+//                        new HandleAddUrl().execute(url, title, description, tags);
                     }
                 })
                 .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
@@ -260,6 +269,28 @@ public class AddActivity extends Activity {
         a_dialogView.findViewById(R.id.loading_title).setVisibility(View.GONE);
     }
 
+    /**
+     * Start the network service to send the link with all its data to Shaarli
+     * @param sharedUrl the one url
+     * @param title a chosen title by the user
+     * @param description user description
+     * @param tags user tags
+     * @param isPrivate true if the link is private
+     * @param account the account which the share operate
+     */
+    private void handleSendPost(String sharedUrl, String title, String description, String tags, boolean isPrivate, ShaarliAccount account){
+        Intent networkIntent = new Intent(this, NetworkService.class);
+        networkIntent.putExtra("action", "postLink");
+        networkIntent.putExtra("sharedUrl", sharedUrl);
+        networkIntent.putExtra("title", title);
+        networkIntent.putExtra("description", description);
+        networkIntent.putExtra("tags", tags);
+        networkIntent.putExtra("privateShare", isPrivate);
+        networkIntent.putExtra("chosenAccountId", account.getId());
+
+        startService(networkIntent);
+        finish();
+    }
     //
     // Class which handle the arrival of a new shared url, async
     //

commit d7c7940f9f9c36a7d4a8e6debee94fc893dc491f

Show toast when link successfully shared
Regression introduced in cd857f2

app/src/main/java/com/dimtion/shaarlier/NetworkService.java
@@ -5,11 +5,14 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
 import android.os.Message;
 import android.os.Messenger;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
+import android.widget.Toast;
 
 import java.io.IOException;
 
@@ -24,12 +27,21 @@ public class NetworkService extends IntentService {
 
     private String loadedTitle;
 
+    private Context mContext;
+    private android.os.Handler mToastHandler;
     private Exception mError;
     private ShaarliAccount mShaarliAccount;
     private String loadedDescription;
 
     public NetworkService() {
         super("NetworkService");
+    }
+
+    @Override
+    public void onCreate(){
+        super.onCreate();
+        mContext = this;
+        mToastHandler = new Handler(Looper.getMainLooper());
     }
 
     @Override
@@ -116,6 +128,22 @@ public class NetworkService extends IntentService {
     }
 
     /**
+     * Display Toast in the main thread
+     * Thanks : http://stackoverflow.com/a/3955826
+     */
+    private class DisplayToast implements Runnable{
+        String mText;
+
+        public DisplayToast(String text){
+            mText = text;
+        }
+
+        public void run(){
+            Toast.makeText(mContext, mText, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
      * Check if the given credentials are correct
      * @param account The account with the credentials
      * @return NO_ERROR if nothing is wrong
@@ -157,10 +185,10 @@ public class NetworkService extends IntentService {
         }
 
         if (!posted) {
-//            Toast.makeText(getApplicationContext(), R.string.add_error + " : " + mError.getMessage(), Toast.LENGTH_LONG).show();
             sendNotificationShareError(sharedUrl, title, description, tags, privateShare);
 //            sendReport(mError, chosenAccount);
         } else {
+            mToastHandler.post(new DisplayToast(getString(R.string.add_success)));
             Log.i("SUCCESS", "Success while sharing link");
         }
     }